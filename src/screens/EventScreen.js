96c72b386932491daf5790e3e2ac9527b9f6edbe
palinayarmolenka <palina.yarmolenka@isfit.no>
2022-10-05 10:54:22 +0200

    initial commit

---
+++ src/screens/EventScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function EventScreen () {
+  return (
+    <View>
+      <Text>Event screen!</Text>
+    </View>
+  )
+}
