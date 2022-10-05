f8c0de8e9d96ffe859a33c4a2eb2eeee02102e78
palinayarmolenka <palina.yarmolenka@isfit.no>
2022-10-05 12:12:50 +0200

    Added bottom navigation tabs

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome5 } from "@expo/vector-icons";
+import EventScreen from './src/screens/EventScreen';
+import MapScreen from './src/screens/MapScreen';
+import ThemeScreen from './src/screens/ThemeScreen';
+import FAQScreen from './src/screens/FAQScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Events" 
+        component={EventScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="map" size={20} color={color} />
+          ),
+        }} />
+        <Tab.Screen name="Theme" 
+        component={ThemeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="map" size={20} color={color} />
+          ),
+        }} />
+        <Tab.Screen name="Map" 
+        component={MapScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="map" size={20} color={color} />
+          ),
+        }} />
+        <Tab.Screen name="FAQ" 
+        component={FAQScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="map" size={20} color={color} />
+          ),
+        }} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
---
+++ src/screens/FAQScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function FAQScreen () {
+  return (
+    <View>
+      <Text>FAQ screen!</Text>
+    </View>
+  )
+}
