9a3105f46359a59bfccd2e0ae21e1ef32beef4af
palinayarmolenka <palina.yarmolenka@isfit.no>
2022-10-06 17:09:49 +0200

    implemented markers in mapScreen

---
+++ src/assets/attractionMarker.js
@@ -0,0 +1,12 @@
+export const attractionMarker = [
+    {
+        key: 'Antikvariatet',
+        title: 'Antikvariatet',
+        info: 'MarkerInfo',
+        information: "In the middle of magical Bakklandet, you can find Antikvariatet, which is probably the most cozy café and bar in Trondheim. You can sit inside surrounded by books looking out on the river, or outside covered in blankets under the heat lamps. Antikvariatet has more kinds of beers than you can imagine, cute cups of coffee (with free refills), and plenty of board games.",
+        latLong: {
+            latitude: 63.42856936726488,
+            longitude: 10.402574056452902,
+        }
+    }
+];
--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import { Text, View, Image, StyleSheet, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import Header from '../components/Header';
+import { attractionMarker } from "../assets/attractionMarker";
+
+const width = Dimensions.get("screen").width;
+const height = Dimensions.get("screen").height;
 
 export default function MapScreen () {
+
+  const markers = [
+    {
+      latitude: 63.42856936726488,
+      longitude: 10.402574056452902,
+      },
+  ];
 
   return (
     <View style={{
@@ -18,8 +29,37 @@ export default function MapScreen () {
         latitudeDelta: 0.062,
         longitudeDelta: 0.05
       }}>
-
+      {
+        markers.map((val) => (
+          <Marker coordinate={{
+            latitude : val.latitude,
+            longitude : val.longitude
+          }}>
+            <Image style={styles.image} source={require("../assets/exploreTrondheim.png")} />
+          </Marker>
+        ))
+      }
       </MapView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  mapStyle: {
+    width: width,
+    height: height,
+  },
+  transparentBox: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: width - 30,
+    backgroundColor: 'transparent',
+  },
+  image: {
+    resizeMode: "contain",
+    width: width * 0.8,
+    height: width * 0.12,
+  },
+});
