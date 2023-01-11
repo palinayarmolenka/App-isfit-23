af6a4f99bbe161338dfd771bb2a81cd7dbb9bfe8
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-01-11 14:26:50 +0100

    Add fetching of samfundet data

--- src/screens/EventScreen.js
+++ src/screens/EventScreen.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+import * as rssParser from "react-native-rss-parser";
 
 export default function EventScreen () {
+
+    const [samfundetEvents, setSamfundetEvents] = useState([])
+
+    const getFeed = async () => {
+      const data = []
+
+      await fetch("http://www.samfundet.no/rss")
+      .then((response) => response.text())
+      .then((responseData) =>
+        //states which type of data we want to use in the app
+        rssParser.parse(responseData, [
+          "body",
+          "agelimit",
+          "location",
+          "category",
+          "link",
+        ])
+      ).then(rss => {
+        rss.items.map((item) => {
+          data.push({
+            title: item.title,
+            desciption: item.description,
+            date: item.published,
+            link: item.links[0].url
+          })
+        })
+      })
+
+      setSamfundetEvents(data)
+    }
+
+  useEffect(() => {
+    getFeed()
+  }, [])
+
   return (
     <View>
-      <Text>Event screen!</Text>
+      <Text>Event screen!asfoihaoigfhioahf</Text>
+      {samfundetEvents.map((event, index) => {
+        return (
+          <View key={index}>
+            <Text>{event.title}</Text>
+            <Text>{event.desciption}</Text>
+            <Text>{event.date}</Text>
+            <Text>{event.link}</Text>
+          </View>
+        )
+      })}
     </View>
   )
 }
