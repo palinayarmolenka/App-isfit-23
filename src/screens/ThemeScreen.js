141e26404a0bd10fba549797af6822e1399d0a39
perkynades <emil.nilsen9888@gmail.com>
2023-01-22 17:06:45 +0100

    Update markers

--- src/screens/ThemeScreen.js
+++ src/screens/ThemeScreen.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { View, ScrollView } from "react-native";
 import Header from "../components/Header";
 import ArticleBox from "../components/ArticleBox";
 import { themeInfo } from "../assets/themeInfo";
 
-export default function ThemeScreen() {
+export default function ThemeScreen({ navigation }) {
+
+  const scrollRef = useRef();
+
+  useEffect(() => {
+        navigation.addListener('focus', () => {
+          scrollRef.current?.scrollTo({
+            y: 0,
+            animated: true,
+          });
+        })
+    }, [navigation])
+
   return (
-    <ScrollView >
+    <ScrollView ref={scrollRef}>
       {themeInfo.map((m, i) =>
         <ArticleBox
           title={m.title}
